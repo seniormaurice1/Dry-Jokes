355ead7fc130052a94ae036bdf8cb666c45d6451
seniormaurice1 <110884669+seniormaurice1@users.noreply.github.com>
2022-12-16 09:17:13 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	dryyyyy();
+	
+	$.getJSON("https://github.com/seniormaurice1", function(view) {
+		$("#page_view_no").html( view.value );
+	});
+
+});
+
+function dryyyyy() {
+
+	var current_joke_number = new Number( $("#joke_number").text() );
+
+	var new_joke_number = current_joke_number + 1;
+	
+	get_joke();
+
+	$("#joke_number").html( new_joke_number );
+}
+
+async function get_joke() {
+
+	const jokesApi = [ 'https://icanhazdadjoke.com/', 'https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?blacklistFlags=nsfw,religious,racist,sexist&type=single' ];
+
+	//const jokesApi = [ 'https://icanhazdadjoke.com/', 'https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?blacklistFlags=nsfw,religious,racist,sexist&type=single', 'https://api.yomomma.info/', 'https://jokes.guyliangilsing.me/retrieveJokes.php?type=yomama', 'https://jokes.guyliangilsing.me/retrieveJokes.php?type=dadjoke' ];
+
+	var random_joke_api = jokesApi[ Math.floor( Math.random() * jokesApi.length ) ];
+
+	$.getJSON( random_joke_api , function(joke_json) {
+		$("#joke").html( joke_json.joke );
+	});
+}
